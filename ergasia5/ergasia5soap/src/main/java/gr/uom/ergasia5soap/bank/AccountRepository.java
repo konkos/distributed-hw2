856252b331391462683c06065f71455436bfb460
Konstantinidis Konstantinos <kon_kostas@windowslive.com>
2022-12-25 02:57:10 +0200

    Delete ergasia5 directory

--- ergasia5/ergasia5soap/src/main/java/gr/uom/ergasia5soap/bank/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package gr.uom.ergasia5soap.bank;
-
-import io.spring.guides.gs_producing_web_service.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<BankAccount, Long> {
-}
